a61f1dc3c250625d96b587d1c4b54140a1dfc188
mattmahoneyrh <mmahoney@redhat.com>
2018-04-12 17:20:45 -0400

    kiali-client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 import yaml
+from kiali import KialiClient
 
 @pytest.fixture(scope="session")
-def config():
+
+def kiali_json():
+
     with open('config/env.yaml', 'r') as yamlfile:
         config = yaml.load(yamlfile)
-    return config
+
+        client = KialiClient(host=config.get('kiali_hostname'),
+                             username=config.get('kiali_username'), password=config.get('kiali_password'))
+
+    return client.graph_namespace(namespace=config.get('mesh_bookinfo_namespace')).to_json_object()
+
--- tests/test_kiali_endpoint.py
+++ tests/test_kiali_endpoint.py
@@ -1,48 +1,28 @@
 import pytest
 import json
-from rest.kiali_api import kiali_api
+from kiali.client import ApiJsonEncoder
 
 # Note: Number of services +1 Views Group Node
 BOOKINFO_EXPECTED_NODES=8
 BOOKINFO_EXPECTED_EDGES=7
 
-def test_service_graph_rest_endpoint(config):
 
-    kiali = kiali_api(url=config.get('kiali_url'),
-                      endpoint=config.get('kiali_service_graph_json_endpoint'),
-                      namespace=config.get('kiali_namespace'))
+def test_service_graph_rest_endpoint(kiali_json):
 
-    response = kiali.get_kiali_service_graph_json(debug=True)
+    assert kiali_json != None, "Json: {}".format(kiali_json)
 
-    validate_response(response)
+    # Validate Node count
+    assert kiali_json.get('elements').get('nodes') >= 1
 
-    # validate node count
-    nodes = json.dumps(response.json.get('elements'))
-    assert json.dumps(nodes, ensure_ascii=False).count('nodes') >= 1
+    # Validate edge count
+    assert kiali_json.get('elements').get('edges') >= 1
 
-    # validate edge count
-    nodes = json.dumps(response.json.get('elements'))
-    assert json.dumps(nodes, ensure_ascii=False).count('edges') >= 1
+def test_service_graph_bookinfo_namespace_(kiali_json):
 
-
-def test_service_graph_bookinfo_namespace(config):
-    kiali = kiali_api(url=config.get('kiali_url'),
-                      endpoint=config.get('kiali_service_graph_json_endpoint'),
-                      namespace=config.get('mesh_bookinfo_namespace'))
-
-    response = kiali.get_kiali_service_graph_json(debug=True)
-
-    validate_response(response)
-
-    # validate node count
-    nodes = json.dumps(response.json.get('elements').get('nodes'))
-    assert json.dumps(nodes, ensure_ascii=False).count('data') == BOOKINFO_EXPECTED_NODES
+    # Validate Node count
+    assert len(kiali_json.get('elements').get('nodes')) == BOOKINFO_EXPECTED_NODES
 
     # validate edge count
-    edges = json.dumps(response.json.get('elements').get('edges'))
-    assert json.dumps(edges, ensure_ascii=False).count('data') == BOOKINFO_EXPECTED_EDGES
-    assert json.dumps(edges, ensure_ascii=False).count('green') == BOOKINFO_EXPECTED_EDGES
-
-def validate_response(response):
-    assert response.status == 200, "Status: {}".format(response.status)
-    assert response.json != None, "Json: {}".format(response.json)
+    edges = kiali_json.get('elements').get('edges')
+    assert len(edges) == BOOKINFO_EXPECTED_EDGES
+    assert json.dumps(edges, ensure_ascii=False, cls=ApiJsonEncoder).count('green') == BOOKINFO_EXPECTED_EDGES
