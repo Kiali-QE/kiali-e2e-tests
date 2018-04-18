783e7c557d5704520a5cc5a754634135b9b40a91
mattmahoneyrh <mmahoney@redhat.com>
2018-04-18 14:42:02 -0400

    New Test Mesh test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,28 @@ import pytest
 import yaml
 from kiali import KialiClient
 
-@pytest.fixture(scope="session")
+ENV_FILE = './config/env.yaml'
 
+@pytest.fixture(scope="session")
 def kiali_json():
 
-    with open('config/env.yaml', 'r') as yamlfile:
-        config = yaml.load(yamlfile)
-
-        client = KialiClient(host=config.get('kiali_hostname'),
-                             username=config.get('kiali_username'), password=config.get('kiali_password'))
+    config = __get_environment_config__(ENV_FILE)
+    client = __get_kiali_client__(config)
 
     return client.graph_namespace(namespace=config.get('mesh_bookinfo_namespace'),
                                   params={'duration': '1m'}).to_json_object()
 
+@pytest.fixture(scope='session')
+def kiali_client():
+    config = __get_environment_config__(ENV_FILE)
+    return __get_kiali_client__(config)
+
+def __get_kiali_client__(config):
+    return KialiClient(host=config.get('kiali_hostname'),
+                       username=config.get('kiali_username'), password=config.get('kiali_password'))
+
+def __get_environment_config__(env_file):
+    with open(env_file) as yamlfile:
+        config = yaml.load(yamlfile)
+
+    return config
