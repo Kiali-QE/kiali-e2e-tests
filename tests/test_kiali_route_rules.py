749ac2f0072a65fe3040a019c0e8a442e392665a
mattmahoneyrh <mmahoney@redhat.com>
2018-05-07 15:07:56 -0400

    Validate JSON does not contain RR

--- tests/test_kiali_route_rules.py
+++ tests/test_kiali_route_rules.py
@@ -1,5 +1,6 @@
 import os
 import conftest
+import time
 
 PARAMS = {'duration': '1m'}
 
@@ -22,15 +23,31 @@ def test_kiali_route_rules(kiali_client):
 
     assert nodes is not None
 
-    route_rule = 0
-    for node in nodes:
-        if 'hasRR' in node["data"] and  node["data"]["hasRR"] == "true":
-            route_rule = route_rule +1
+    #route_rule_count = get_route_rule_count(kiali_client, environment_configmap)
 
-    assert route_rule is 2
+    assert get_route_rule_count(kiali_client, environment_configmap) == 2
 
     delete_command_text = "oc delete routerule " + route_rule_configmap['metadata']['name'] + " -n " +  environment_configmap.get('mesh_bookinfo_namespace')
 
     delete_command_result = os.popen(delete_command_text).read()
 
     assert delete_command_result.__contains__("deleted")
+
+    time.sleep(10)
+
+    # Validate that JSON no longer has Route Rules
+    assert get_route_rule_count(kiali_client, environment_configmap) == 0
+
+def get_route_rule_count(kiali_client, environment_configmap):
+
+    nodes = kiali_client.graph_namespace(namespace=environment_configmap.get('mesh_bookinfo_namespace'), params=PARAMS)[
+        "elements"]['nodes']
+
+    assert nodes is not None
+
+    route_rule_count = 0
+    for node in nodes:
+        if 'hasRR' in node["data"] and node["data"]["hasRR"] == "true":
+            route_rule_count = route_rule_count + 1
+
+    return route_rule_count
