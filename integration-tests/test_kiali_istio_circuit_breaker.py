61a440d53ca36d8af9a6d871426ac94128005919
mattmahoneyrh <mmahoney@redhat.com>
2018-07-18 16:29:57 -0400

    Ingetration tests

---
+++ integration-tests/test_kiali_istio_circuit_breaker.py
@@ -0,0 +1,42 @@
+import os
+import conftest
+from utils.timeout import timeout
+import time
+
+PARAMS = {'duration': '1m'}
+
+def test_kiali_circuit_breakers(kiali_client):
+    environment_configmap = conftest.__get_environment_config__(conftest.ENV_FILE)
+    circuit_breaker_configmap = conftest.__get_environment_config__(conftest.CIRCUIT_BREAKER_FILE)
+
+    add_command_text = "oc apply -n " + environment_configmap.get('mesh_bookinfo_namespace') + " -f " + os.path.abspath(os.path.realpath(conftest.CIRCUIT_BREAKER_FILE))
+
+    add_command_result = os.popen(add_command_text).read()
+
+    assert add_command_result.__contains__("created") or add_command_result.__contains__("configured")
+
+    graph = kiali_client.graph_namespace(namespace=environment_configmap.get('mesh_bookinfo_namespace'), params=PARAMS)
+
+    assert graph is not None
+
+    with timeout(seconds=60, error_message='Timed out waiting for Circuit Breaker to be Created'):
+        while True:
+            nodes = kiali_client.graph_namespace(namespace=environment_configmap.get('mesh_bookinfo_namespace'), params=PARAMS)["elements"]['nodes']
+
+            assert nodes is not None
+
+            circuit_breaker = 0
+            for node in nodes:
+                if 'hasCB' in node["data"] and  node["data"]["hasCB"]:
+                    circuit_breaker = circuit_breaker +1
+
+            if circuit_breaker > 0:
+                break
+
+            time.sleep(1)
+
+    delete_command_text = "oc delete destinationrule " + circuit_breaker_configmap['metadata']['name'] + " -n " +  environment_configmap.get('mesh_bookinfo_namespace')
+
+    delete_command_result = os.popen(delete_command_text).read()
+
+    assert delete_command_result.__contains__("deleted")
---
+++ integration-tests/test_kiali_istio_endpoint.py
@@ -0,0 +1,27 @@
+import pytest
+import json
+
+# Note: Number of services +1 Views Group Node
+BOOKINFO_EXPECTED_NODES=8
+BOOKINFO_EXPECTED_EDGES=7
+
+
+def test_service_graph_rest_endpoint(kiali_json):
+
+    assert kiali_json != None, "Json: {}".format(kiali_json)
+
+    # Validate Node count
+    assert len(kiali_json.get('elements').get('nodes')) >= 1
+
+    # Validate edge count
+    assert len(kiali_json.get('elements').get('edges')) >= 1
+
+def test_service_graph_bookinfo_namespace_(kiali_json):
+
+    # Validate Node count
+    assert len(kiali_json.get('elements').get('nodes')) == BOOKINFO_EXPECTED_NODES
+
+    # validate edge count
+    edges = kiali_json.get('elements').get('edges')
+    assert len(edges) == BOOKINFO_EXPECTED_EDGES
+    #assert json.dumps(edges, ensure_ascii=False, cls=ApiJsonEncoder).count('green') == BOOKINFO_EXPECTED_EDGES
